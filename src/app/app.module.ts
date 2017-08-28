d6aa874c67ef0c21cc2dfe1942ccdcbe0f13d0d8
Rafael Prado <rafaeldacruzdoprado@hotmail.com>
2017-08-28 10:27:20 -0300

    Adicionando componetes de mensagens e grids de pessoa e lançamentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from "@angular/forms";
 import { NgModule } from '@angular/core';
+
 
 import { InputTextModule } from 'primeng/components/inputtext/inputtext';
 import { ButtonModule } from 'primeng/components/button/button';
 import { DataTableModule } from 'primeng/components/datatable/datatable';
 import { TooltipModule } from 'primeng/primeng';
+import { InputTextareaModule } from "primeng/components/inputtextarea/inputtextarea";
+import { CalendarModule } from "primeng/components/calendar/calendar";
+import { SelectButtonModule } from "primeng/components/selectbutton/selectbutton";
+import { DropdownModule } from "primeng/components/dropdown/dropdown";
+import { InputMaskModule } from "primeng/components/inputmask/inputmask";
+
+import { CurrencyMaskModule } from "ng2-currency-mask";
 
 import { AppComponent } from './app.component';
 import { LancamentoPesquisaComponent } from './lancamento-pesquisa/lancamento-pesquisa.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { PessoaPesquisaComponent } from './pessoa-pesquisa/pessoa-pesquisa.component';
 import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
-import { InputTextareaModule } from "primeng/components/inputtextarea/inputtextarea";
+import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
+import { MessageComponent } from './message/message.component';
+import { LancamentosGridComponent } from './lancamentos-grid/lancamentos-grid.component';
+import { PessoasGridComponent } from './pessoas-grid/pessoas-grid.component';
+
+
 
 @NgModule({
   declarations: [
@@ -19,15 +34,26 @@ import { InputTextareaModule } from "primeng/components/inputtextarea/inputtexta
     LancamentoPesquisaComponent,
     NavBarComponent,
     PessoaPesquisaComponent,
-    LancamentoCadastroComponent
+    LancamentoCadastroComponent,
+    PessoaCadastroComponent,
+    MessageComponent,
+    LancamentosGridComponent,
+    PessoasGridComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
     InputTextModule,
     ButtonModule,
     DataTableModule,
     TooltipModule,
-    InputTextareaModule
+    InputTextareaModule,
+    CalendarModule,
+    SelectButtonModule,
+    DropdownModule,
+    InputMaskModule,
+    CurrencyMaskModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/lancamento-cadastro/lancamento-cadastro.component.ts
+++ src/app/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -7,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LancamentoCadastroComponent implements OnInit {
 
+  tipos = [
+    {label:'Receita', value:'RECEITA'},
+    {label:'Despesa', value:'DESPESA'},
+  ];
+
+  categorias = [
+    {label:'Alimentação', value:'1'},
+    {label:'Viagens', value:'2'},
+    {label:'Carro', value:'3'},
+  ];
+
+  pessoas = [
+    {label:'João Silva', value:'3'},
+    {label:'Maria Lima', value:'1'},
+    {label:'Pedroa alvaro', value:'5'},
+  ];
   constructor() { }
 
   ngOnInit() {
